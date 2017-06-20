b85407a805fc5b31fc7164c6c678533916658d4f
Edgar Juarez <ejjt.686@gmail.com>
2017-06-19 17:26:07 -0700

    Turned Nodelist into arrays and seperated minutes and seconds

---
+++ script.js
@@ -0,0 +1,6 @@
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+const seconds = timeNodes.map(node => node.dataset.time).map(timeCode => {
+    const [mins, secs] = timeCode.split(':');
+    console.log(mins,secs);
+})

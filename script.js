81891ce120a6cee545346fe3245459e39237b983
Edgar Juarez <ejjt.686@gmail.com>
2017-06-19 17:46:54 -0700

    App now adds all video time from the html and converts it into hours,minutes and seconds

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const seconds = timeNodes.map(node => node.dataset.time).map(timeCode => {
     return (mins * 60) + secs;
     console.log(mins,secs);
 }).reduce((total, vidSeconds) => total + vidSeconds);
+
+let secondsLeft = seconds;
+
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, mins, secondsLeft);
